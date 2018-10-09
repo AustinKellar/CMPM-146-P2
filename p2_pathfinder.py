68369d00359023dc4d4aeee981346af34f8345e6
Austin Kellar <ajk@eduroam-169-233-142-126.ucsc.edu>
2018-10-09 14:27:38 -0700

    A*

--- p2_pathfinder.py
+++ p2_pathfinder.py
@@ -1,3 +1,4 @@
+from queue import PriorityQueue
 from math import inf, sqrt
 
 def matching_box(point, mesh):
@@ -11,11 +12,17 @@ def midpoint(box):
     x1, x2, y1, y2 = box
     return ((x1+x2)/2, (y1+y2)/2)
 
+def heuristic(box1, box2):
+    x1, y1 = midpoint(box1)
+    x2, y2 = midpoint(box2)
+
+    return abs(x2 - x1) + abs(y2 - y1)
+
 def distance(box1, box2):
     x1, y1 = midpoint(box1)
     x2, y2 = midpoint(box2)
 
-    return sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    return sqrt(((x2 - x1)**2) + ((y2 - y1)**2))
 
 def assemble_path(source_box, destination_box, boxes):
     path = []
@@ -29,15 +36,6 @@ def assemble_path(source_box, destination_box, boxes):
         current_box = boxes.get(current_box, None)
     return path
 
-def pop_lowest_distance(queue, distances):
-    min = inf
-    min_index
-    for index, box in queue:
-        if distances[box] < min:
-            min = distances[box]
-            min_index = index
-    return queue.pop(min_index)
-
 def find_path (source_point, destination_point, mesh):
     """    Searches for a path from source_point to destination_point through the mesh    Args:        source_point: starting point of the pathfinder        destination_point: the ultimate goal the pathfinder must reach        mesh: pathway constraints the path adheres to
     Returns:
@@ -46,7 +44,8 @@ def find_path (source_point, destination_point, mesh):
     source_box = matching_box(source_point, mesh)
     destination_box = matching_box(destination_point, mesh)
 
-    queue = [ source_box ]
+    queue = PriorityQueue()
+    queue.put(source_box, 0)
     boxes = {}
     boxes[source_box] = None
     distances = {}
@@ -56,15 +55,16 @@ def find_path (source_point, destination_point, mesh):
 
     distances[source_box] = 0
 
-    while len(queue) > 0:
-        current = queue.pop(0)
+    while not queue.empty():
+        current = queue.get()
         if current == destination_box:
             break
         for next in mesh['adj'][current]:
             new_distance = distances[current] + distance(current, next)
             if next not in distances or new_distance < distances[next]:
                 distances[next] = new_distance
-                queue.append(next)
+                priority = new_distance + heuristic(next, destination_box)
+                queue.put(next, priority)
                 boxes[next] = current
 
     path = assemble_path(source_box, destination_box, boxes)
